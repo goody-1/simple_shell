eea580e1d512e60f00528e5fb713c9b0e819db09
goody-1 <atundeg@gmail.com>
2024-02-23 16:33:13 +0100

    remove printf statements and move things around a bit

--- concept/super_simple.c
+++ concept/super_simple.c
@@ -7,8 +7,6 @@ int main(void)
 	size_t num_words;
 	pid_t ppid = getpid();
 
-	printf("Parent pid is %u\n", ppid);
-
 	if (isatty(STDIN_FILENO)) /* check if program is running interactively */
 	{
 		while (1)
@@ -18,27 +16,33 @@ int main(void)
 			list[num_words] = NULL;
 
 			handle_exec(list, str, ppid);
+			if (list)
+				free_list(list);
+			if (str)
+				free(str);
 		}
 	}
 	else
 	{ /* program is running in non-interactive mode */
 		FILE *input_file = stdin;
-		char *line;
 
-		while ((line = read_line(input_file)) != NULL)
+		while ((str = read_line(input_file)) != NULL)
 		{
-			line[strcspn(line, "\n")] = '\0';
-
-			str = strdup(line);
 			list = split_string(str, " \t\n", &num_words);
 			list[num_words] = NULL;
 
 			handle_exec(list, str, ppid);
+
+			if (list)
+				free_list(list);
+			if (str)
+				free(str);
 		}
 	}
-
-	free_list(list);
-	free(str);
+	if (list)
+		free_list(list);
+	if (str)
+		free(str);
 	return (0);
 }
 
@@ -48,30 +52,29 @@ void handle_exec(char **list, char *str, int ppid)
 	pid_t child = fork();
 
 	if (child == -1)
-		handle_exit(list, str, ppid, EXIT_FAILURE, "fork", no_kill);
+		handle_exit(list, str, ppid, EXIT_FAILURE, "fork", terminate);
 
 	if (child == 0)
 	{
-		printf("My pid is %u and my parent pid is %u\n", getpid(), ppid);
-
 		 /* case where input to getline function is -1: CTRL + D */
-		if (!list[0])
+		if (!list || !list[0])
 			handle_exit(list, NULL, ppid, EXIT_SUCCESS, NULL, terminate);
+
+		if (strcmp(list[0], "exit") == 0)
+			handle_exit(list, str, ppid, EXIT_SUCCESS, NULL, terminate);
 
 		if (strcmp(list[0], "env") == 0 || strcmp(list[0], "printenv") == 0)
 		{
 			print_environment();
 			handle_exit(list, str, ppid, EXIT_SUCCESS, NULL, no_kill);
 		}
-		if (strcmp(list[0], "exit") == 0)
-			handle_exit(list, str, ppid, EXIT_SUCCESS, NULL, terminate);
 
 		/* handle commands */
 		if (execve(list[0], list, environ) == -1)
 			handle_exit(list, str, ppid, EXIT_FAILURE, "./shell", no_kill);
 	}
-	else
-		wait(&status); /* wait for child process to finish */
+
+	wait(&status); /* wait for child process to finish */
 }
 
 void print_environment(void)
