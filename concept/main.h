dff2efc4988f15d057d5a85c201da324e584ea17
goody-1 <atundeg@gmail.com>
2024-02-23 09:43:17 +0100

    organize function prototypes

--- concept/main.h
+++ concept/main.h
@@ -12,14 +12,28 @@
 
 extern char **environ;
 
+/* core functions */
 char **split_string(const char *str, const char *delimiters,
 					size_t *num_words);
-char *prompt(void);
-void perror_mem_alloc(void);
-char *trim_newline(char *str);
-void free_list(char **list);
 char **word_list(char *str, char *delim);
-void handle_exit(int signum __attribute__((unused)));
+void handle_exec(char **list, char *str, int ppid);
+
+/*** helper functions ***/
+/* defined in simple*/
 void print_environment(void);
 
-#endif
+/* defined in split_str */
+void perror_mem_alloc(char *error);
+char *trim_newline(char *str);
+
+/* defined in utilities */
+void free_list(char **list);
+void handle_kill(int signum __attribute__((unused)));
+void handle_exit(char **list, char *str, int ppid,
+					int exit_status, char *p_err, int terminate);
+
+/* defined in prompt */
+char *prompt(void);
+char *read_line(FILE *input);
+
+#endif /* MAIN_H */
