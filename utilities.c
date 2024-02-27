0e82f92f7c9a5570f6b61fdc0fff937674c89cc7
king-luminous <albashir97@gmail.com>
2024-02-27 13:37:48 -0800

    description

--- utilities.c
+++ utilities.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * free_list - function to free list.
+ * @list: a pointer to the list to be freed.
+ * Return: NULL
+ */
+
 void free_list(char **list)
 {
 	size_t i = 0;
@@ -15,6 +21,15 @@ void free_list(char **list)
 	free(list);
 }
 
+/**
+ * handle_exit - function that terminates the programme
+ * @list: a pointer to the list
+ * @exit_status: terminate a process
+ * @str: pointer to a string
+ * @ppid: parent process id
+ * @terminate: terminates a process
+ * @p_err: descriptive message to identify error
+ */
 void handle_exit(char **list, char *str, int ppid,
 					int exit_status, char *p_err, int terminate)
 {
@@ -30,6 +45,10 @@ void handle_exit(char **list, char *str, int ppid,
 		perror(p_err);
 	exit(exit_status);
 }
+/**
+ * handle_kill - function used in terminating an initiatted process
+ * @signum: unique identifier
+ */
 void handle_kill(int signum __attribute__((unused)))
 {
 	exit(EXIT_SUCCESS);
