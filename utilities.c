14a3b8b845a089efa2d2e7f5eeea80deffdd2ce7
goody-1 <atundeg@gmail.com>
2024-03-01 09:57:58 +0100

    add helper functions _atoi and is_integer

--- utilities.c
+++ utilities.c
@@ -53,3 +53,51 @@ void handle_kill(int signum __attribute__((unused)))
 {
 	exit(EXIT_SUCCESS);
 }
+
+/**
+ * is_integer - function to test whether a string contains numbers only
+ * @s: string to be tested
+ *
+ * Return: 0 if at least a character is not digit, 1 if all are digits
+*/
+int is_integer(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+* _atoi - convert a string to an integer.
+* @s: source pointer
+* Return: void
+*/
+
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned int res = 0;
+	int flag = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] == '-')
+			flag *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + (s[i] - '0');
+			if (s[i + 1] == ' ')
+				break;
+		}
+
+		i++;
+	}
+	return (flag * res);
+}
