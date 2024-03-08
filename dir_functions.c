74e996273bc68beacd8f1cb4af903f6ba87b0c54
goody-1 <atundeg@gmail.com>
2024-03-08 13:33:34 +0100

    remove unwanted printf statements

--- dir_functions.c
+++ dir_functions.c
@@ -8,26 +8,25 @@
  */
 int change_directory(const char *directory)
 {
-    char current_dir[1024];
+	char current_dir[1024];
 
-    if (getcwd(current_dir, sizeof(current_dir)) == NULL)
-    {
-        perror("cwd");
-        return (-1);
-    }
+	if (getcwd(current_dir, sizeof(current_dir)) == NULL)
+	{
+		perror("cwd");
+		return (-1);
+	}
 
-    if (chdir(directory) == 0)
-    {
-        printf("Directory changed successfully.\n");
-        setenv("OLDPWD", current_dir, 1); /* Update OLDPWD environment variable */
-        setenv("PWD", directory, 1); /* Update PWD environment variable */
-        return (0);
-    }
-    else
-    {
-        perror("dir");
-        return (-1);
-    }
+	if (chdir(directory) == 0)
+	{
+		setenv("OLDPWD", current_dir, 1); /* Update OLDPWD environment variable */
+		setenv("PWD", directory, 1); /* Update PWD environment variable */
+		return (0);
+	}
+	else
+	{
+		perror("dir");
+		return (-1);
+	}
 }
 
 /**
@@ -38,39 +37,30 @@ int change_directory(const char *directory)
  */
 int cd(const char *directory)
 {
-    if (directory == NULL)
-    {
-        directory = getenv("HOME");
-        if (directory == NULL)
-        {
-            printf("No home directory available.\n");
-            return (-1);
-        }
-        change_directory(directory);
-        return (0);
-    }
+	if (directory == NULL)
+	{
+		directory = getenv("HOME");
+		if (directory == NULL)
+			return (-1);
 
-    if (strcmp(directory, "-") == 0)
-    {
-        char *previous_dir = getenv("OLDPWD");
-        if (previous_dir == NULL)
-        {
-            printf("No previous directory available.\n");
-            return (-1);
-        }
-        if (change_directory(previous_dir) == 0)
-        {
-            printf("Directory changed to previous directory: %s\n", previous_dir);
-            return (0);
-        }
-        else
-        {
-            printf("Failed to change directory to previous directory: %s\n", previous_dir);
-            return (-1);
-        }
-    }
-    else
-    {
-        return (change_directory(directory));
-    }
+		change_directory(directory);
+		return (0);
+	}
+
+	if (strcmp(directory, "-") == 0)
+	{
+		char *previous_dir = getenv("OLDPWD");
+
+		if (previous_dir == NULL)
+			return (-1);
+
+		if (change_directory(previous_dir) == 0)
+			return (0);
+		else
+			return (-1);
+
+	}
+	else
+		return (change_directory(directory));
+
 }
